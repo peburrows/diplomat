3e2416e8b63d66d65fad4ffe0234479f00d666be
Phil Burrows <github@philburrows.com>
2018-04-11 20:02:56 -0500

    up the minimum Elixir version to 1.4

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Diplomat.Mixfile do
     [
       app: :diplomat,
       version: "0.10.0",
-      elixir: "~> 1.3",
+      elixir: "~> 1.4",
       description: "A library for interacting with Google's Cloud Datastore",
       package: package(),
       deps: deps(),
